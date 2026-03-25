f65cbe86f2a2546a8d6f3ee0d5bad595e41b6edb
Neurocylcq <biolinua@qq.com>
2026-03-25 10:31:27 +0800

    Deploy to GitHub Pages

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/Volumes/KINGSTON/WorkSpace/Project/Web/neurocylcq.github.io/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/Volumes/KINGSTON/WorkSpace/Project/Web/neurocylcq.github.io/node_modules/infima/dist/css/default/default.css"),
+  require("/Volumes/KINGSTON/WorkSpace/Project/Web/neurocylcq.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Volumes/KINGSTON/WorkSpace/Project/Web/neurocylcq.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Volumes/KINGSTON/WorkSpace/Project/Web/neurocylcq.github.io/src/css/custom.css"),
+];
